c7e3053d03d370737ce4001e8b2f8eb678b879a2
berserkcoder <shreyanshagrawalae2@gmail.com>
2025-12-07 08:54:15 +0530

    Time: 111 ms (32.00%) | Memory: 199 MB (33.38%) - LeetSync

---
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int countTrapezoids(vector<vector<int>>& points) {
+        long long int ans = 0;
+        map<int,int> store;
+        for(int i = 0;i<points.size();i++){
+            store[points[i][1]]++;
+        }
+        vector<long long int> imp;
+        for(auto it : store){
+            if(it.second > 1){
+                long long int n = it.second-1;
+                long long int val = (n*(n+1))/2;
+                imp.push_back(val);
+            }
+        }
+        if(imp.size() <= 1) return ans;
+        vector<long long int> prefix(imp.size());
+        int sum = 0;
+        for(int i = imp.size()-1;i>=0;i--){
+            sum += imp[i];
+            prefix[i] = sum;
+        }
+        for(int i = 0;i<imp.size()-1;i++){
+            ans += (long long int)((long long int)imp[i] * (long long int)prefix[i+1]) % 1000000007;
+        }
+        return ans % 1000000007;
+    }
+};
