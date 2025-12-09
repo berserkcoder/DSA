ae17fc38f64735e8ac4692d78b7d35dbc074c73e
berserkcoder <shreyanshagrawalae2@gmail.com>
2025-12-10 00:16:02 +0530

    Time: 558 ms (75.91%) | Memory: 367.8 MB (67.52%) - LeetSync

--- 3885-count-special-triplets/count-special-triplets.cpp
+++ 3885-count-special-triplets/count-special-triplets.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int specialTriplets(vector<int>& nums) {
-        map<int,long long int> left;
-        map<int,long long int> right;
+        unordered_map<int,long long int> left;
+        unordered_map<int,long long int> right;
         long long int ans = 0;
         long long int mod = 1000000007;
         for(int i = 0;i<nums.size();i++){
@@ -11,12 +11,12 @@ public:
         for(int i = 0;i<nums.size();i++){
             right[nums[i]]--;
             long target = 2 * nums[i];
-            // if(left.count(target) && right.count(target) && right[target] > 0){
-            //     ans = (ans + (left[target] * right[target]) % mod) % mod;
-            // }
-            if(left.find(nums[i]*2) != left.end() && right.find(nums[i]*2) != right.end()){
+            if(left.count(target) && right.count(target) && right[target] > 0){
                 ans = (ans + (left[target] * right[target]) % mod) % mod;
             }
+            // if(left.find(nums[i]*2) != left.end() && right.find(nums[i]*2) != right.end()){
+            //     ans = (ans + (left[target] * right[target]) % mod) % mod;
+            // }
             left[nums[i]]++;
         }
         return ans;
