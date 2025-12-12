b2ee92b4730578dc71881d66dba06edd8c688523
berserkcoder <shreyanshagrawalae2@gmail.com>
2025-12-13 03:44:54 +0530

    Time: 4 ms (52.71%) | Memory: 27 MB (27.61%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -3,11 +3,12 @@ public:
     string firstPalindrome(vector<string>& words) {
         string s = "";
         for(int i = 0;i<words.size();i++){
-            s = "";
-            for(int j = 0;j<words[i].size();j++){
-                s += words[i][j];
-            }
+            s = words[i];
+            // for(int j = 0;j<words[i].size();j++){
+            //     s += words[i][j];
+            // }
             if(isPalindrome(s)) return s;
+            s = "";
         }
         return "";
     }
