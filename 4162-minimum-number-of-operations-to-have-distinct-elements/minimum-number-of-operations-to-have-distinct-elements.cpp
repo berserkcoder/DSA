3b9280d8a38ef6a95e6c655319568ce2a41f6748
berserkcoder <shreyanshagrawalae2@gmail.com>
2025-12-22 12:04:13 +0530

    Time: 783 ms (7.14%) | Memory: 265.9 MB (7.14%) - LeetSync

---
+++ 4162-minimum-number-of-operations-to-have-distinct-elements/minimum-number-of-operations-to-have-distinct-elements.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        map<int,int> mpp;
+        int l = 0;
+        int r = 0;
+        int ans = 0;
+        while(r < nums.size()){
+            mpp[nums[r]]++;
+            while(mpp[nums[r]] == 2){
+                ans++;
+                for(int i = 0;i<=2;i++){
+                    if(l >= nums.size()) return ans;
+                    mpp[nums[l]]--;
+                    l++;
+                }
+                // if(l > r+1) r = l;
+            }
+            r++;
+        }
+        return ans;
+    }
+};
