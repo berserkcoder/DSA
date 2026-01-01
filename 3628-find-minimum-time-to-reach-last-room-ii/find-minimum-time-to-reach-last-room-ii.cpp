3ac2112391522816e03a01cc481de854dff1bdaf
berserkcoder <shreyanshagrawalae2@gmail.com>
2026-01-01 14:42:25 +0530

    Time: 564 ms (45.66%) | Memory: 135.7 MB (44.90%) - LeetSync

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        int n = moveTime.size()-1;
+        int m = moveTime[0].size()-1;
+        vector<vector<int>> vis(n+1,vector<int> (m+1,0));
+        pq.push({0,0,0,0});
+        vis[0][0] = 1;
+        int ans = INT_MAX;
+        // bool flag = false;
+        while(!pq.empty()){
+            int val = pq.top()[0];
+            int row = pq.top()[1];
+            int col = pq.top()[2];
+            int flag = pq.top()[3];
+            pq.pop();
+            if(row == n && col == m){
+                if(ans > val) ans = val;
+                return ans;
+            }
+            if(row > 0 && vis[row-1][col] == 0){
+                int time = moveTime[row-1][col];
+                if(flag) {
+                    time = max(time+2,val+2);
+                    // flag = !flag;
+                }
+                else {
+                    time = max(time+1,val+1);
+                    // flag = !flag;
+                }
+                pq.push({time,row-1,col,!flag});
+                vis[row-1][col] = 1;
+            }
+            if(row < n && vis[row+1][col] == 0){
+                int time = moveTime[row+1][col];
+                if(flag) {
+                    time = max(time+2,val+2);
+                    // flag = !flag;
+                }
+                else {
+                    time = max(time+1,val+1);
+                    // flag = !flag;
+                }
+                pq.push({time,row+1,col,!flag});
+                vis[row+1][col] = 1;
+            }
+            if(col > 0 && vis[row][col-1] == 0){
+                int time = moveTime[row][col-1];
+                if(flag) {
+                    time = max(time+2,val+2);
+                    // flag = !flag;
+                }
+                else {
+                    time = max(time+1,val+1);
+                    // flag = !flag;
+                }
+                pq.push({time,row,col-1,!flag});
+                vis[row][col-1] = 1;
+            }
+            if(col < m && vis[row][col+1] == 0){
+                int time = moveTime[row][col+1];
+                if(flag) {
+                    time = max(time+2,val+2);
+                    // flag = !flag;
+                }
+                else {
+                    time = max(time+1,val+1);
+                    // flag = !flag;
+                }
+                pq.push({time,row,col+1,!flag});
+                vis[row][col+1] = 1;
+            }
+        }
+        return ans;
+    }
+};
