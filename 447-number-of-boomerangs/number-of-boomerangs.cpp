b6ffdb22272afd7ac414248ab31d07259e65b989
berserkcoder <shreyanshagrawalae2@gmail.com>
2026-01-16 03:33:39 +0530

    Time: 360 ms (51.33%) | Memory: 127.9 MB (50.82%) - LeetSync

---
+++ 447-number-of-boomerangs/number-of-boomerangs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfBoomerangs(vector<vector<int>>& points) {
+        int res = 0, n = points.size();
+        for (int i = 0; i < n; ++i) {
+            unordered_map<int, int> map;
+            for (int j = 0; j < n; ++j) {
+                if (i == j) continue;
+                int dx = points[i][0] - points[j][0];
+                int dy = points[i][1] - points[j][1];
+                int distSquared = dx * dx + dy * dy;
+                map[distSquared]++;
+            }
+            for (auto& [dist, count] : map) res += count * (count - 1);
+        }
+        return res;
+    }
+};
