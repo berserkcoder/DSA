5155c002a157a51048dddf12ded7787259e390e3
berserkcoder <shreyanshagrawalae2@gmail.com>
2026-01-20 11:16:50 +0530

    Time: 4 ms (23.67%) | Memory: 32.5 MB (7.10%) - LeetSync

--- 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> minBitwiseArray(vector<int>& nums) {
-        vector<int> store(200000,-1);
+        vector<int> store(2000,-1);
         for(int i = 1;i<=1000;i++){
             int val = i | (i+1);
             if(store[val] == -1)store[val] = i;
