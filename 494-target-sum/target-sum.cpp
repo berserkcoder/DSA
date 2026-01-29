d1f49de43f34474980b28764a66414202ad8a8e4
berserkcoder <shreyanshagrawalae2@gmail.com>
2026-01-30 02:25:57 +0530

    Time: 3 ms (85.93%) | Memory: 15.3 MB (56.12%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -8,8 +8,20 @@ public:
         if(abs(target) > total) return 0;
         if((target+total) % 2 != 0) return 0;
         int val = (target+total)/2;
-        vector<vector<int>> dp(nums.size(),vector<int>(val+1,-1));
-        return sign(nums.size()-1,nums,val,0,dp);
+        vector<vector<int>> dp(nums.size(),vector<int>(val+1,0));
+        dp[0][0] = (nums[0] == 0 ? 2 : 1);
+        if (nums[0] != 0 && nums[0] <= val)
+            dp[0][nums[0]] = 1;
+        for(int i = 1;i<nums.size();i++){
+            for(int j = 0;j<val+1;j++){
+                int take = 0;
+                if(j >= nums[i]) take = dp[i-1][j-nums[i]];
+                int nottake = dp[i-1][j];
+                dp[i][j] = take + nottake;
+            }
+        }
+        return dp[nums.size()-1][val];
+        // return sign(nums.size()-1,nums,val,0,dp);
     }
     int sign(int ind,vector<int>& arr,int t,int sum,vector<vector<int>>& dp){
         if(sum > t) return 0;
