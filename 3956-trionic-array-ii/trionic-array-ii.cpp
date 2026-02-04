6921ced71be95309a2bb0214edfd9594b7abd97a
berserkcoder <shreyanshagrawalae2@gmail.com>
2026-02-04 11:51:31 +0530

    Time: 199 ms (12.73%) | Memory: 228.3 MB (11.82%) - LeetSync

---
+++ 3956-trionic-array-ii/trionic-array-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long maxSumTrionic(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<long long>> dp(n+1,vector<long long>(4,-1e17-1));
+        for(int i = 1;i<=n;i++){
+            dp[i][0] = nums[i-1];
+        }
+        for(int j = 1;j<=3;j++){
+            dp[1][j] = -1e17-1;
+        }
+        long long ans = -1e17-1;
+        for(int i = 2;i<=n;i++){
+            for(int j = 1;j<=3;j++){
+                if( (j == 1 || j == 3) && nums[i-1] > nums[i-2]) {
+                    dp[i][j] = max(nums[i-1] + dp[i-1][j-1],nums[i-1] + dp[i-1][j]);
+                    if(j == 3) ans = max(ans,dp[i][j]);
+                }else if(j == 2 && nums[i-1] < nums[i-2]) {
+                    dp[i][j] = max(nums[i-1] + dp[i-1][j-1],nums[i-1] + dp[i-1][j]);
+                }
+            }
+        }
+        return ans;
+    }
+};
