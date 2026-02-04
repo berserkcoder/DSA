89fb6e32611cc27e73441069c9d742a8f1916389
berserkcoder <shreyanshagrawalae2@gmail.com>
2026-02-04 18:33:07 +0530

    Time: 228 ms (7.27%) | Memory: 228.5 MB (9.09%) - LeetSync

--- 3956-trionic-array-ii/trionic-array-ii.cpp
+++ 3956-trionic-array-ii/trionic-array-ii.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     long long maxSumTrionic(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<long long>> dp(n+1,vector<long long>(4,-1e17-1));
+        vector<vector<long long>> dp(n+1,vector<long long>(4,-1000000000000000));
         for(int i = 1;i<=n;i++){
             dp[i][0] = nums[i-1];
         }
-        for(int j = 1;j<=3;j++){
-            dp[1][j] = -1e17-1;
-        }
-        long long ans = -1e17-1;
+        long long ans = -1000000000000000;
         for(int i = 2;i<=n;i++){
             for(int j = 1;j<=3;j++){
-                if( (j == 1 || j == 3) && nums[i-1] > nums[i-2]) {
+                if((j == 1 || j == 3)  && nums[i-1] > nums[i-2]){
                     dp[i][j] = max(nums[i-1] + dp[i-1][j-1],nums[i-1] + dp[i-1][j]);
-                    if(j == 3) ans = max(ans,dp[i][j]);
-                }else if(j == 2 && nums[i-1] < nums[i-2]) {
+                    if(j==3) ans = max(ans,dp[i][j]);
+                }
+                if(j == 2 && nums[i-1] < nums[i-2]){
                     dp[i][j] = max(nums[i-1] + dp[i-1][j-1],nums[i-1] + dp[i-1][j]);
                 }
             }
