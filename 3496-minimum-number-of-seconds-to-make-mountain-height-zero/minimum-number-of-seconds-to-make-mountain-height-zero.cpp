a439f635601087e5924c9343fc53562e17ff77f5
berserkcoder <shreyanshagrawalae2@gmail.com>
2026-03-13 17:55:59 +0530

    Time: 979 ms (5.16%) | Memory: 112.6 MB (6.45%) - LeetSync

---
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        long long ans = 0;
+        int n = workerTimes.size();
+        priority_queue<vector<long long>,vector<vector<long long>>,greater<vector<long long>>> pq;
+        for(int i = 0;i<n;i++){
+            pq.push({workerTimes[i],workerTimes[i],1});
+        } 
+        int store = mountainHeight;
+        while(mountainHeight > 0){
+            long long val = pq.top()[1];
+            long long days = pq.top()[2];
+            long long current = pq.top()[0];
+            pq.pop();
+            // cout << val << " " << days << endl;
+            long long temp = val * (days+1);
+            pq.push({current+temp,val,days+1});
+            mountainHeight--;
+        }
+        while(store > 0){
+            long long val = pq.top()[1];
+            long long days = pq.top()[2];
+            long long current = pq.top()[0];
+            pq.pop();
+            cout << current << " " << val << " " << days << endl;
+            if(days != 1)ans = max(ans,current-(val*days));
+            store -= days-1;
+        }
+        return ans;
+    }
+};
