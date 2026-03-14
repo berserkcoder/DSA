ca7cedd230cec27a74eebbcc2af8029ea558b6eb
berserkcoder <shreyanshagrawalae2@gmail.com>
2026-03-14 22:18:57 +0530

    Time: 481 ms (22.08%) | Memory: 275 MB (78.35%) - LeetSync

---
+++ 4253-minimum-cost-to-equalize-arrays-using-swaps/minimum-cost-to-equalize-arrays-using-swaps.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minCost(vector<int>& nums1, vector<int>& nums2) {
+        map<int,int> store;
+        int n = nums1.size();
+        for(int i = 0;i<n;i++){
+            store[nums1[i]]++;
+            store[nums2[i]]--;
+            if(store[nums2[i]] == 0) store.erase(nums2[i]);
+        }
+        int ans = 0;
+        for(auto it : store){
+            if(it.second % 2 != 0) return -1;
+            if(it.second < 0) ans -= it.second;
+            else ans += it.second;
+        }
+        return ans/4;
+    }
+};
