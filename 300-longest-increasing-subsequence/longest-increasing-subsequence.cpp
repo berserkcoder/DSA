467f913e4af9989ad212e0bfd94a2a4a36821941
berserkcoder <shreyanshagrawalae2@gmail.com>
2026-04-20 18:27:58 +0530

    Time: 279 ms (28.05%) | Memory: 296.4 MB (17.38%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> dp(nums.size(),vector<int>(20002,-1));
-        int ans = recursive(0,nums,-10001,dp);
-        return ans;
-    }
-    int recursive(int ind,vector<int>& nums,int last,vector<vector<int>>& dp){
-        if(ind == nums.size()) return 0;
-        if(dp[ind][last+10001] != -1) return dp[ind][last+10001];
-        if(nums[ind] > last) return dp[ind][last+10001] = max(1+recursive(ind+1,nums,nums[ind],dp),recursive(ind+1,nums,last,dp));
-        return dp[ind][last+10001] = recursive(ind+1,nums,last,dp);
+        int n = nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i = n-1;i>=0;i--){
+            for(int j = i-1;j>=-1;j--){
+                int len = 0 + dp[i+1][j+1];
+                if(j == -1 || nums[i] > nums[j]){
+                    len = max(len,1+dp[i+1][i+1]);
+                }
+                dp[i][j+1] = len;
+            }
+        }
+        return dp[0][0];
     }
 };
