f1576676fcb450269e16f4cb932f539db54c0d08
berserkcoder <shreyanshagrawalae2@gmail.com>
2026-06-12 11:33:02 +0530

    Time: 27 ms (3.45%) | Memory: 8.7 MB (43.21%) - LeetSync

--- 4319-sum-of-compatible-numbers-in-range-i/sum-of-compatible-numbers-in-range-i.cpp
+++ 4319-sum-of-compatible-numbers-in-range-i/sum-of-compatible-numbers-in-range-i.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int sumOfGoodIntegers(int n, int k) {
         int ans = 0;
-        for(int i = 0;i<=10000;i++){
+        for(int i = 0;i<=1000;i++){
             if(abs(n-i) <= k && (n&i) == 0) {
                 ans += i;
                 cout << i << " ";
