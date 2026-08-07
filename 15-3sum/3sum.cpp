a8e414bb87c7d84565316af8ff06b606e67e7c4d
berserkcoder <shreyanshagrawalae2@gmail.com>
2026-08-08 00:48:27 +0530

    Time: 331 ms (9.64%) | Memory: 41 MB (9.57%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        set<vector<int>> st;
+        for(int i = 0;i<nums.size();i++){
+            vector<int> store;
+            store.push_back(nums[i]);
+            int l = i+1;
+            int h = nums.size()-1;
+            while(l < h){
+                int sum = nums[l] + nums[h];
+                int val = -nums[i];
+                if(sum == val){
+                    store.push_back(nums[l]);
+                    store.push_back(nums[h]);
+                    sort(store.begin(),store.end());
+                    st.insert(store);
+                    store.pop_back();
+                    store.pop_back();
+                    l++;
+                    h--;
+                }
+                else if(sum > val){
+                    h--;
+                }
+                else l++;
+            }
+        }
+        for(auto it : st){
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
